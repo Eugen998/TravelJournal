3da40a7da8b34eb3fa95c7e91caff663fb57ec15
Eugen998 <costineugen98@gmail.com>
2019-01-14 19:27:46 +0200

    date pickers done

--- app/src/main/java/com/example/eugen/traveljournal/ManageTripActivity.java
+++ app/src/main/java/com/example/eugen/traveljournal/ManageTripActivity.java
@@ -1,13 +1,59 @@
 package com.example.eugen.traveljournal;
 
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
-public class ManageTripActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.time.Year;
+import java.util.Calendar;
+
+public class ManageTripActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_trip);
+        Button from = findViewById(R.id.fromdate);
+        Button to = findViewById(R.id.todate);
+        from.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"from date");
+                i=0;
+            }
+        });
+        to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"to date");
+                i=1;
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR,year);
+        c.set(Calendar.MONTH,month);
+        c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String date = DateFormat.getDateInstance().format(c.getTime());
+        Button b1 = findViewById(R.id.fromdate);
+        Button b2 = findViewById(R.id.todate);
+        if(i==0){
+            b1.setText(date);
+        }
+        else{
+            b2.setText(date);
+        }
     }
 }
